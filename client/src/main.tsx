cafe3ba9ced4fdad8c506acf941e319e850f428f
TheOCGroup <gocasiorealtor@gmail.com>
2026-08-19 00:59:02 -0500

    chore(client): load OCG motion layer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./ocg-motion.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
